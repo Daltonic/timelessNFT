d2cca2b8ec4c3cbc1d415c448e9f1af97add4e8e
Daltonic <darlingtongospel@gmail.com>
2023-06-16 13:13:00 +0100

    Resolves artwork not showing bug

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,3 @@
-// require('babel-register')
-// require('babel-polyfill')
-require('dotenv').config()
-const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 module.exports = {
   // Configure networks (Localhost, Rinkeby, etc.)
@@ -10,15 +6,6 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*', // Match any network id
-    },
-    goerli: {
-      provider: () =>
-        new HDWalletProvider(process.env.SECRET_KEY, process.env.ENDPOINT_URL),
-      network_id: 5,
-      gas: 5500000,
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
   },
   contracts_directory: './src/contracts/',
